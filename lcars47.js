11a43322ae51e1aed1215cc48922f72b8c8f5ee6
SkyeRangerDelta <chessmasterhutchison@gmail.com>
2018-04-09 18:13:52 -0400

    V47.6.0.00 | Second Great Reprogramming
    Updated GitIgnore and restructured vulnerability patch. Experimental code now includes externalized directories.

--- lcars47.js
+++ lcars47.js
@@ -11,135 +11,74 @@ const Discord = require('discord.js');
 const lcars = new Discord.Client();
 const fs = require('fs');
 
-const commands = require('./Commands');
+const system = require('./lcars_subsystem.json');
+const ch = require('./Subsystems/subs_channels.json');
 
-console.log(commands);
+const prefix = system.prefix;
+const lcarsColor = "#f4eb42"
+const lcarsAlertColor = "#d3150e"
 
-const prefix = "!";
+var lcarsVersion = system.version;
 
-const thumbnail = './thumbnail.png'
-const mainColor = "#f4eb42";
-const emerColor = "#d3150e";
+//SESSION RECORDING & ENGINEERING MODE
+let engmode;
+var session;
 
-const lcarsVersion = "47.5.2.00";
+var SUBS_Engm = JSON.parse(fs.readFileSync("./Subsystems/subs_engmode.json", "utf8"));
+engmode = SUBS_Engm.engmode;
 
-//GLOBAL SYSTEM VARIABLES
-let engmode = false;
+var SUBS_Sess = JSON.parse(fs.readFileSync("./Subsystems/subs_session.json", "utf8"));
+session = SUBS_Sess.sessionnum++;
+fs.writeFileSync("./Subsystems/subs_session.json", JSON.stringify(SUBS_Sess));
 
-//STARTUP PROCEDURES
-    //SESSION RECORDING SYSTEM
-    var lcarsVarsDoc = JSON.parse(fs.readFileSync('./lcars_vars.json', 'utf8'));
-    var lcarsSessionNum = lcarsVarsDoc.sessionnum++;
+function engm() {
+    engmode = !engmode;
+}
 
-    console.log("[SESSION#] " + lcarsSessionNum);
+//MESSAGE AND COMMAND SYSTEM HANDLER
+lcars.on("message", msg => {
+    if (msg.author.bot) return
+    if (msg.content.indexOf(prefix) !== 0) return
 
-    fs.writeFileSync("./lcars_vars.json", JSON.stringify(lcarsVarsDoc));
+    const cmd = msg.content.slice(prefix.length).trim().split(/ +/g);
+    const cmdID = cmd.shift().toLowerCase();
 
-//READY EVENT HANDLER
-lcars.on('ready', () => {
+    try {
+        let cmdFile = require(`./Commands/${cmdID}.js`);
+        cmdFile.run(lcars, msg, cmd);
+    } catch (err) {
+        console.error(err);
+    }
+});
 
-    //Channel Definitions
-    var serverAnnouncements = lcars.channels.get('341454723771138048');
+//SYSTEM STARTUP
 
-    //Startup Embed
+//Channels
+var engdeckID = lcars.channels.get(ch.ch_engdeck);
+
+//Responder
+lcars.on("ready", () => {
     var startupseq = new Discord.RichEmbed();
-        startupseq.setTitle("o0o...LCARS47 STARTUP...o0o");
-        startupseq.setColor(mainColor);
+        startupseq.setTitle("o0o - LCARS SYSTEM STARTUP - o0o");
+        startupseq.setColor(lcarsColor);
         startupseq.setDescription(
-            "Booting LCARS from isolinear storage...\n"+
-            "Initializing holosystems...\n"+
-            "LCARS now booted, performing checks...\n"+
-            "=============================================\n" +
-            "LCARS READY | V" + lcarsVersion + " ready and running.\n"+
-            "LCARS Session: " + lcarsSessionNum
+            "LCARS Shipboard Operating System\n"+
+            "Version " + lcarsVersion + " on session:" + session + ".\n"+
+            "===================================\n"+
+            "Booting from isolinear storage...\n"+
+            "LCARS47 is now online."
         );
 
-    console.log("LCARS V" + lcarsVersion + " booted successfully.");
-    console.log("================================================");
-    console.log("[SESSION#] " + lcarsSessionNum);
-    console.log("[ENG-MODE] Setting: " + engmode);
+    console.log("LCARS V" + lcarsVersion + " | System Startup");
+    console.log("====================================");
+    console.log("[SESSION#] " + session);
 
     lcars.user.setActivity("!help | V" + lcarsVersion);
 
-    serverAnnouncements.send({embed: startupseq}).then(sent => sent.delete(20000));
+    console.log(engdeckID);
+    console.log("JSON: " + ch.ch_engdeck);
 
-
+    engdeckID.send({embed: startupseq}).then(sent => sent.delete(30000));
 });
 
-//COMMANDS AND CORE SYSTEMS
-lcars.on('message', async msg => {
-    if (!msg.member || msg.member.id === lcars.user.id) return
-
-    let args = msg.content.substring(prefix.length).split(' ');
-    let cmd = commands[args.splice(0, 1)[0]]; 
-
-    console.log(args);
-    console.log(cmd);
-
-    if (!cmd) {
-        return
-    }
-
-    cmd.run({args, lcars, msg})
-    .then((t) => {
-        msg.channel.send(t);
-    });
-
-    //INTER-SYSTEM VARIABLES
-
-    //FUNCTION SYSTEMS
-    //Command Structure
-    /*function commandis(cmd, msg) {
-        return msg.content.startsWith(prefix + cmd);
-    }*/
-
-    //COMMANDS (Sorted Alphabetically)
-        //Engineering Mode
-        //IPs
-        //Version
-        //Status
-
-
-    //COMMAND EMBEDS
-    //Version
-    var version = new Discord.RichEmbed();
-        version.setTitle("o0o...VERSION REPORT...o0o");
-        version.setColor(mainColor);
-        version.setDescription(
-            "LCARS is currently running on version:\n`" + lcarsVersion +
-            "`\n\nSee SkyeRangerDelta or any Dev Team member for information on LCARS systems or development."
-        );
-
-    //Engineering Mode
-    var engembed = new Discord.RichEmbed();
-        engembed.setTitle("o0o...LCARS SESSION STATUS REPORT...o0o");
-        engembed.setColor(mainColor);
-        engembed.setDescription(
-            "LCARS self-diagnostics report for session " + lcarsSessionNum + ".\n"+
-            ""
-        );
-
-
-    //COMMAND CALLS
-    //Engineering Mode
-    /*if (commandis("engm", msg)) {
-        msg.delete();
-
-        if (msg.member.roles.find('name', 'Dev Team')) {
-            engm();
-        }
-        else {
-            msg.reply("You don't have the necessary permissions to toggle ENGM!");
-        }
-    }
-
-    //Version
-    if (commandis("version", msg)) {
-        msg.delete();
-
-        msg.channel.send({embed: version}).then(sent => sent.delete(30000));
-    }*/
-
-});
-
-lcars.login('MzQxNDI1NzE1MjEzNzYyNTgy.DGBAeA.ZegiYgeOdilwAmLijQJja3CNBjc');
+lcars.login(system.token);
