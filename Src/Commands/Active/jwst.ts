29b6eb3c722970c2120916dedbab8132642f2af6
SkyeRangerDelta <whutchison@boxlogixautomation.com>
2024-04-11 08:13:46 -0500

    Refactor: Lint corrections for JWST

--- Src/Commands/Active/jwst.ts
+++ Src/Commands/Active/jwst.ts
@@ -1,57 +1,56 @@
 // -- STATUS --
 
 // Imports
-import { type ChatInputCommandInteraction, CommandInteraction } from 'discord.js';
+import { type ChatInputCommandInteraction, Message } from 'discord.js';
 import { SlashCommandBuilder } from '@discordjs/builders';
 import { type LCARSClient } from '../../Subsystems/Auxiliary/LCARSClient.js';
 
 import https from 'https';
 
 import Utility from '../../Subsystems/Utilities/SysUtils.js';
-import dotenv from 'dotenv';
 
 // Functions
 const data = new SlashCommandBuilder()
-  .setName('jwst')
-  .setDescription('Accesses the James Webb Space Telescope API.');
+  .setName( 'jwst' )
+  .setDescription( 'Accesses the James Webb Space Telescope API.' );
 
-data.addSubcommand(s => s
-  .setName('status')
-  .setDescription('Retrieves version/status data about the JWST API.')
+data.addSubcommand( s => s
+  .setName( 'status' )
+  .setDescription( 'Retrieves version/status data about the JWST API.' )
 );
 
-data.addSubcommand(s => s
-  .setName('random')
-  .setDescription('Pulls a random image from MAST from a specific JWST program.')
-  .addIntegerOption(o => o
-    .setName('program-id')
-    .setDescription('The ID of the JWST program to pull from.')
-    .addChoices([['NGC 3324 (Carina)', 2731], ["Stephan's Quintet", 2732], ['NGC 3132 (Southern Ring Nebula)', 2733], ['WASP-96b & HAT-P-18b Exoplanets', 2734]])
-    .setRequired(true)
+data.addSubcommand( s => s
+  .setName( 'random' )
+  .setDescription( 'Pulls a random image from MAST from a specific JWST program.' )
+  .addIntegerOption( o => o
+    .setName( 'program-id' )
+    .setDescription( 'The ID of the JWST program to pull from.' )
+    .addChoices( [['NGC 3324 (Carina)', 2731], ["Stephan's Quintet", 2732], ['NGC 3132 (Southern Ring Nebula)', 2733], ['WASP-96b & HAT-P-18b Exoplanets', 2734]] )
+    .setRequired( true )
   )
 );
 
-async function execute (LCARS47: LCARSClient, int: ChatInputCommandInteraction): Promise<void> {
-  Utility.log('info', '[JWST] Received a JWST inquiry command.');
+async function execute ( LCARS47: LCARSClient, int: ChatInputCommandInteraction ): Promise<void> {
+  Utility.log( 'info', '[JWST] Received a JWST inquiry command.' );
   await int.deferReply();
 
   const cmd = int.options.getSubcommand();
-  Utility.log('info', `[JWST] Processing ${cmd} query...`);
+  Utility.log( 'info', `[JWST] Processing ${cmd} query...` );
 
-  switch (cmd) {
+  switch ( cmd ) {
     case 'status':
-      await doJWSTRequest('/', int);
+      await doJWSTRequest( '/', int );
       break;
     case 'random':
-      await doJWSTRequest(`/program/id/${int.options.getInteger('program-id')}`, int);
+      await doJWSTRequest( `/program/id/${int.options.getInteger( 'program-id' )}`, int );
       break;
     default:
       break;
   }
 }
 
-async function doJWSTRequest (reqPath: string, int: ChatInputCommandInteraction): Promise<void> {
-  if (int.options.getSubcommand() === 'status') {
+async function doJWSTRequest ( reqPath: string, int: ChatInputCommandInteraction ): Promise<void> {
+  if ( int.options.getSubcommand() === 'status' ) {
     const options = {
       method: 'GET',
       hostname: 'api.jwstapi.com',
@@ -60,34 +59,36 @@ async function doJWSTRequest (reqPath: string, int: ChatInputCommandInteraction)
       maxRedirects: 20
     };
 
-    const req = https.request(options, (res) => {
-      Utility.log('info', '[JWST] Sending REQ.');
+    const req = https.request( options, ( res ) => {
+      Utility.log( 'info', '[JWST] Sending REQ.' );
 
-      res.on('data', (chunk) => {
-        Utility.log('info', '[JWST] Request Data:\n' + chunk);
-        const data = JSON.parse(chunk);
-        if (data.statusCode === 401) {
-          int.editReply({
+      res.on( 'data', ( chunk: Buffer ) => {
+        Utility.log( 'info', '[JWST] Request Data:\n' + chunk.toString() );
+        const data = JSON.parse( chunk.toString() );
+        if ( data.statusCode === 401 ) {
+          void int.editReply( {
             content: 'Reject: Invalid API key.'
-          });
-        } else if (data.statusCode === 200) {
-          int.editReply({
-            content: data.body
-          });
+          } );
         }
-      });
+        else if ( data.statusCode === 200 ) {
+          void int.editReply( {
+            content: data.body
+          } );
+        }
+      } );
 
-      res.on('end', () => {
-        Utility.log('info', '[JWST] Request Ended.');
-      });
+      res.on( 'end', () => {
+        Utility.log( 'info', '[JWST] Request Ended.' );
+      } );
 
-      res.on('error', (err) => {
-        Utility.log('err', '[JWST] Request Error:\n' + err);
-      });
-    });
+      res.on( 'error', ( err ) => {
+        Utility.log( 'err', '[JWST] Request Error:\n' + err.message );
+      } );
+    } );
 
     req.end();
-  } else {
+  }
+  else {
     const options = {
       method: 'GET',
       hostname: 'api.jwstapi.com',
@@ -98,43 +99,43 @@ async function doJWSTRequest (reqPath: string, int: ChatInputCommandInteraction)
       maxRedirects: 20
     };
 
-    // @ts-expect-error
-    const chunks = [];
+    const chunks: Buffer[] = [];
 
-    const req = await https.request(options, (res) => {
-      Utility.log('info', `[JWST] Sending REQ (${options.hostname + options.path}).`);
+    const req = https.request( options, ( res ) => {
+      Utility.log( 'info', `[JWST] Sending REQ (${options.hostname + options.path}).` );
 
-      res.on('data', (chunk) => {
-        Utility.log('info', '[JWST] Receiving additional chunks.');
-        chunks.push(chunk);
-      });
+      res.on( 'data', ( chunk: Buffer ) => {
+        Utility.log( 'info', '[JWST] Receiving additional chunks.' );
+        chunks.push( chunk );
+      } );
 
-      res.on('end', async () => {
-        Utility.log('info', '[JWST] Request Ended.');
-        // @ts-expect-error
-        const resData = Buffer.concat(chunks);
-        const data = JSON.parse(resData.toString());
+      res.on( 'end', () => {
+        Utility.log( 'info', '[JWST] Request Ended.' );
+        const resData = Buffer.concat( chunks );
+        const data = JSON.parse( resData.toString() );
         const records = [];
 
-        for (const entry in data.body) {
-          if (data.body[entry].file_type === 'jpg') {
-            records.push(data.body[entry]);
+        for ( const entry in data.body ) {
+          if ( data.body[entry].file_type === 'jpg' ) {
+            records.push( data.body[entry] );
           }
         }
 
-        const rEntryIndex = Math.floor(Math.random() * records.length);
+        const rEntryIndex = Math.floor( Math.random() * records.length );
         const rEntry = records[rEntryIndex];
 
-        await int.editReply({
+        void int.editReply( {
           content: `[${rEntry.program}] ${rEntry.id}\nDesc: ${rEntry.details.description}`,
           files: [`${rEntry.location}`]
-        });
-      });
+        } ).then( ( msg: Message ) => {
+          console.log( 'JWST message edited.' );
+        } );
+      } );
 
-      res.on('error', (err) => {
-        Utility.log('err', '[JWST] Request Error:\n' + err);
-      });
-    });
+      res.on( 'error', ( err ) => {
+        Utility.log( 'err', '[JWST] Request Error:\n' + err.message );
+      } );
+    } );
 
     req.end();
   }
